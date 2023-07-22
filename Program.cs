22bb1f4e1cdc53671cca42cdf70238af84cbc04f
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-22 11:54:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSL_Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"D:\Raven\Practice Exercise\C#\OSL_Assignment1\Rainbow School.txt";
+                string[] lines = File.ReadAllLines(path);
+                Console.WriteLine("The Students data are \n");
+                int index = 1;
+                foreach (string line in lines)
+                    Console.WriteLine($"  {index++} : "+line);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("All the Students data in the files are displayed.");
+            }
+        }
+    }
+}
